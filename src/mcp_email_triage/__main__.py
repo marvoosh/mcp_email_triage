1cb9feb8cdc2f71c48a7f56420b7ba2de57f9bcf
Marva Noah <marvo@MacBook-Pro.local>
2025-12-10 11:07:20 +0200

    Making sure main.py is running the main function

--- src/mcp_email_triage/__main__.py
+++ src/mcp_email_triage/__main__.py
@@ -1,4 +1,4 @@
-
+from mcp_email_triage.server import mcp
 
 if __name__ == "__main__":
-    pass
+    mcp.run()
--- src/mcp_email_triage/server.py
+++ src/mcp_email_triage/server.py
@@ -1,1 +1,13 @@
 from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("email_triage")
+
+
+@mcp.tool()
+def emails_list() -> list:
+    """List of emails to be triaged."""
+
+    return emails_list
+
+
+
